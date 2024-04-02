e9fc7717bbe9cf7c41e0859261dff1de3ab75ccb
OjedaErwin123 <159177988+OjedaErwin123@users.noreply.github.com>
2024-04-02 12:56:44 -0400

    commit 1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,6 +6,16 @@ using namespace std;
 
 void exercise_1(string s1) {
   // TODO: YOUR CODE HERE
+    int x,y,lon;
+   
+   for(int i= 0; i<s1.size(); ++i){
+       if(s1[i]== ' '){
+       cout <<i<<endl;
+  
+       }
+   }
+   
+   cout<<s1.size()<<endl;
 }
 
 void exercise_2(string s1) {
