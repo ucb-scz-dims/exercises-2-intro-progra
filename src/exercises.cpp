7f4abdd543eb71c0de6298979a1550b0ff744de4
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-04-05 23:17:21 -0400

    2

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,11 +6,44 @@ using namespace std;
 
 void exercise_1(string s1) {
   // TODO: YOUR CODE HERE
+  string str;
+    getline(cin, str); // Leer la línea desde la entrada estándar
+
+    int indice = 0; // Inicializar el índice
+    for (int i = 0; i < str.size(); i++) { // Iterar sobre cada carácter de la línea
+        if (str[i] == ' ') { // Si el carácter es un espacio, imprimir el índice
+            cout << indice << " ";
+        }
+        indice++; // Incrementar el índice para la siguiente letra
+    }
+    // Imprimir el índice después del último carácter de la línea
+    cout << indice << endl;
+
+
+    return 0;
 }
 
 void exercise_2(string s1) {
   // TODO: YOUR CODE HERE
+        string word;
+
+    for (char c : s1) {
+        if (c != ' ') {
+            word += c;
+        } else {
+            if (!word.empty()) {
+                cout << "[" << word << "]" << endl;
+                word = "";
+            }
+        }
+    }
+
+    // Si hay una palabra al final de la cadena, imprímela
+    if (!word.empty()) {
+        cout << "[" << word << "]" << endl;
+    }
 }
+
 
 void exercise_3(string s1) {
   // TODO: YOUR CODE HERE
