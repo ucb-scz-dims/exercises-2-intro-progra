6b7dddc5c526ab5e814847a845a44542a86e782b
Nicole <142690531+NicoleLozadaLeon@users.noreply.github.com>
2024-04-06 20:57:41 -0400

    ex6 first commit

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -66,7 +66,9 @@ void exercise_5(int n, int k) {
 
 int exercise_6(int n) {
   // TODO: YOUR CODE HERE
-  return 0;
+  while (n != 0) {
+        sum += n; 
+        cout << sum; 
 }
 
 void exercise_7(int n) {
