4ab3fa4e8ecc83cb0cd48881ca70dde6f385cbe5
Nicole <142690531+NicoleLozadaLeon@users.noreply.github.com>
2024-04-11 09:15:55 -0400

    ex8 delete commit

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -96,9 +96,6 @@ void exercise_7(int n) {
 
 void exercise_8(string s) {
   string s_char = "";
-  //if( s_char == ""){
-  //       cout << "YES" << endl;
-  // }
     for(int i = 0; i < s.size(); ++i){
         if(s[i] != ' '){
             s_char += s[i];
