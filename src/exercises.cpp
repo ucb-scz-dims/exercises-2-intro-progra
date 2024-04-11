eec28b3b2e9c0979908e1c95321777ffc4b3df03
Nicole <142690531+NicoleLozadaLeon@users.noreply.github.com>
2024-04-11 09:15:33 -0400

    ex8 add for

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -96,9 +96,9 @@ void exercise_7(int n) {
 
 void exercise_8(string s) {
   string s_char = "";
-  if( s_char == ""){
-         cout << "YES" << endl;
-   }
+  //if( s_char == ""){
+  //       cout << "YES" << endl;
+  // }
     for(int i = 0; i < s.size(); ++i){
         if(s[i] != ' '){
             s_char += s[i];
@@ -112,6 +112,11 @@ void exercise_8(string s) {
             cout << "NO" << endl;
             break;
         }
+    for (int i = 0 ; i < s_char.size(); ++i){if( s_char == ""){
+        cout << "YES" << endl;
+         break;
+    }
+    }
     }
   // TODO: YOUR CODE HERE
 }
