84751df24fe290cffa3c191a0db4abe44d4a5c41
Nicole <142690531+NicoleLozadaLeon@users.noreply.github.com>
2024-04-13 23:13:12 -0400

    ex 15 add if

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -212,7 +212,10 @@ string exercise_14(int n) {
 void exercise_15(int decimal) {
   // TODO: YOUR CODE HERE
   string bin = ""; 
-  while (decimal >= 0) {
+  if(decimal == 0){
+    bin = to_string(decimal);
+  }
+  while (decimal > 0) {
   int r = decimal % 2; 
   bin = to_string(r) + bin; 
 
