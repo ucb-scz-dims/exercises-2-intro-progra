24956eb369d04af3e9ef4ef3ed10b5161aba3722
Nicole <142690531+NicoleLozadaLeon@users.noreply.github.com>
2024-04-14 17:10:56 -0400

    ex 5 if else

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -63,41 +63,24 @@ void exercise_4(int n) {
 }
 
 void exercise_5(int n, int k) {
-  int day_one = 1;
+ for (int i = 1; i < n; ++i) {
+  cout << "   ";
+ }for(int i = 1; i<= k ; ++i){
+  if(j<10){
+    cout << " ";
+  }
+  cout << i;
 
- for (int i = 1; i <= 6; ++i) {
-     if (i == 1) {
-   for (int i = 1; i < n; ++i) {
-   cout << "   ";
-        }
-   for (int o = n; o <= 7; ++o) {
-      if (day_one < 10) {
-          cout << " ";
-         }
-        cout << day_one++;
-          cout << " ";
-        if (day_one > k) {
-            break;
-          }
-         }
-        cout << endl;
-        } else {
-   for (int i = 1; i <= 7; ++i) {
-         if (day_one < 10) {
-         cout << " ";
-       }
-          cout << day_one++;
-          cout << " ";
-           if (day_one > k) {
-           break;
-           }
-         }
-       cout << endl;
-        }
-        if (day_one > k) {
-            break;
-        }
-    }
+  int new_line = ( i + n -1) % 7;
+  if(salto == 0){
+    cout << endl;
+  } else if( i == k){
+    cout << " " << endl;
+  } else {
+    cout << " ";
+  }
+ }
+
 }
 
 int exercise_6(int n) {
